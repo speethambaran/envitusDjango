141d135dcb658477175dd34cdd3a4c13d26046cb
shilpa <shilpakayam@gmail.com>
2022-10-26 16:30:19 +0530

    view changed

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('register/', views.register, name='register'),
-    # path('login/', views.login_view, name='login_view'),
-    # path('user_role/', views.user_role, name='user_role'),
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
 
 ]
